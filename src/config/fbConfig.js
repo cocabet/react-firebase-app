a029bc4abc6ed3ae1eb233b900a54c840fda8f44
Oscar <cocabet@gmail.com>
2021-06-27 20:51:40 -0500

    Change data

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
- apiKey: "AIzaSyDFJhL_kiiO0J8r12MD4DMCN5A9CQREqp0",
- authDomain: "net-blogplan.firebaseapp.com",
- projectId: "net-blogplan",
- storageBucket: "net-blogplan.appspot.com",
- messagingSenderId: "742469833854",
- appId: "1:742469833854:web:5f1007a5b4c47626db342f"
+ apiKey: "xyxyxyxyxyyxyxyx",
+ authDomain: "test-testing.firebaseapp.com",
+ projectId: "net-testing",
+ storageBucket: "net-testing.appspot.com",
+ messagingSenderId: "7424698555533854444",
+ appId: "1:742469833854:web:507a5b4c476242f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
